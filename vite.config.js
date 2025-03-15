ba422edd506a4dc9b900ec992d08cb684c058759
laura.baluchova <laura.menova@gmail.com>
2025-03-15 22:07:25 +0100

    comment out build on config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/',
-  build: {
-    rollupOptions: {
-      input: '/index.html',
-    },
-  },  
+  // build: {
+  //   rollupOptions: {
+  //     input: '/index.html',
+  //   },
+  // },  
 });
