bcef61e4c74e7d4c7b7ce63312f3671592677a3f
laura.baluchova <laura.menova@gmail.com>
2025-03-15 17:22:05 +0100

    add import

--- modules/paintingData.js
+++ modules/paintingData.js
@@ -1,6 +1,7 @@
+import painting1 from '../src/assets/1.jpg';
 export const paintingData = [
   // Front Wall
- {imgSrc: `../src/artworks/1.jpg`, 
+ {imgSrc: painting1, 
   width: 5, // width of the painting
   height: 4, // height of the painting
   position: { x: -15 + 10 * 0, y: 2, z: -19.5 }, // position of the painting
