b745b8c555d76a4306a06e086d1e7af371d5c65c
laura.baluchova <laura.menova@gmail.com>
2025-03-15 14:02:40 +0100

    update config to make json file with dist content

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
-  base: '/',
   build: {
+    outDir: 'dist',
     rollupOptions: {
-      input: '/index.html',
-    },
-  },
+      input: 'index.html',
+      output: {
+        banner: () => {
+          const files = fs.readdirSync('dist').map(file => ({
+            name: file,
+            content: fs.readFileSync(path.join('dist', file), 'utf-8'),
+          }));
+          fs.writeFileSync('dist/file-log.json', JSON.stringify(files, null, 2));
+          return '';
+        }
+      }
+    }
+  }
 });
