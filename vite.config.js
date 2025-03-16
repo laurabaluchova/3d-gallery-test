2d003dc6ce9a7ccc6968009437678d70ff6be6d3
laura.baluchova <laura.menova@gmail.com>
2025-03-16 14:17:29 +0100

    add / to new base

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/3d-gallery-test',
+  base: '/3d-gallery-test/',
   build: {
     rollupOptions: {
       input: '/index.html',
